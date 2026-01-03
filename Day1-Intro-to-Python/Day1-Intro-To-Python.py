966218d461ccb1dffcc16966a248a4f7c50c9f62
TALHA1000RR <mvfdg8628@gmx.us>
2026-01-02 20:55:09 -0700

    Add Day 1: Introduction to Python

---
+++ Day1-Intro-to-Python/Day1-Intro-To-Python.py
@@ -0,0 +1,6 @@
+# Day 1: Introduction to Python
+
+# Simple print statements to show what we learned
+print("What is Programming?")
+print("Programming is giving instructions to a computer to perform tasks.")
+
