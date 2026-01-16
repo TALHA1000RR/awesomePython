6b3134a56d8e226aedaa8fabcbdfb10db51cd922
TALHA1000RR <mvfdg8628@gmx.us>
2026-01-16 16:51:50 -0700

    Add Day 11: Exercise

---
+++ Day11-Exercise/Day11-Exercise.py
@@ -0,0 +1,30 @@
+
+# 1. Data Types
+x = 10
+y = 3.14
+z = "Python"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 2. Type Casting
+num = input("Enter a number: ")
+num = int(num)
+print(num + 5)
+
+# 3. Operators
+a = 10
+b = 3
+
+print(a + b, a - b, a * b, a / b)
+print(a > b and b < 5)
+
+# 4. If-Else
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
