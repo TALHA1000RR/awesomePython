bef53e86338dc57261817452499952ce02966a18
TALHA1000RR <mvfdg8628@gmx.us>
2026-01-17 14:11:09 -0700

    Day 19: For & While Loops

---
+++ Day19-For-&-While-Loop/Day19-For-&-While-Loops.py
@@ -0,0 +1,27 @@
+# Day 19 - Loops in Python
+
+# WHILE LOOP 
+
+print("While Loop Example:")
+
+num = 1
+while num <= 3:
+    print("Hello from while loop")
+    break # For stop the infinite loop
+
+x = 1
+while x == 1:
+    print("Python is easy")
+    break
+
+
+# FOR LOOP 
+
+print("\nFor Loop:")
+
+for item in ["A", "B"]:
+    print("Learning Python")
+    
+
+for x in [1, 2, 3]:
+    print("Hello from for loop")
