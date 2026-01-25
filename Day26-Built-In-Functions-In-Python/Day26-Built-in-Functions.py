db6bb6877218cff1d623cc56ba2ee1ee40a91614
TALHA1000RR <mvfdg8628@gmx.us>
2026-01-25 04:54:03 -0700

    Day 26: Built-in Functions in Python

---
+++ Day26-Built-In-Functions-In-Python/Day26-Built-in-Functions.py
@@ -0,0 +1,11 @@
+# Day 26: Built-in Functions in Python
+
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+print("Length:", len(numbers))
+print("Type:", type(numbers))
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sum:", sum(numbers))
+
