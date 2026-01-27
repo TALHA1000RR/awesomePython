2675ceae6579a6c5dfadcf9da2af15902e107995
TALHA1000RR <mvfdg8628@gmx.us>
2026-01-27 15:07:15 -0700

    Day 28: String Slicing Practice

---
+++ Day28-String-Slicing-Practice/Day28-String-Slicing-Practice.py
@@ -0,0 +1,7 @@
+text = "LearningPython"
+
+print(text[0:8])     # Learning
+print(text[8:])      # Python
+print(text[:5])      # Learn
+print(text[-6:])     # Python
+print(text[2:10])    # arningP
