692903dd139e322435318fac9534f579764a5828
TALHA1000RR <mvfdg8628@gmx.us>
2026-02-20 17:03:37 +0500

    Day 49: Polymorphism

---
+++ Day49-Polymorphism/Day49-Polymorphism.py
@@ -0,0 +1,67 @@
+# Day 49: Polymorphism in Python
+
+# Example 1: Same method, different classes
+class Dog:
+    def speak(self):
+        print("Dog barks")
+
+
+class Cat:
+    def speak(self):
+        print("Cat meows")
+
+
+animals = [Dog(), Cat()]
+
+for animal in animals:
+    animal.speak()   # same method name
+
+
+print("\n--- Example 2 ---")
+
+# Polymorphism with inheritance
+
+class Animal:
+    def speak(self):
+        print("Animal sound")
+
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog bark")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat meow")
+
+
+animals = [Dog(), Cat(), Animal()]
+
+for a in animals:
+    a.speak()
+
+
+print("\n--- Example 3 ---")
+
+# Real-life Example
+
+class Shape:
+    def area(self):
+        print("Area not defined")
+
+
+class Rectangle(Shape):
+    def area(self):
+        print("Area = length × width")
+
+
+class Circle(Shape):
+    def area(self):
+        print("Area = π × r²")
+
+
+shapes = [Rectangle(), Circle()]
+
+for s in shapes:
+    s.area()
