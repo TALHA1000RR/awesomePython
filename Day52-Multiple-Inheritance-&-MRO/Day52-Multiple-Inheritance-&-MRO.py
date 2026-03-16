3eb205ce15608d67e971d3f0249270c4ea12e4ec
TALHA1000RR <mvfdg8628@gmx.us>
2026-03-16 15:32:14 +0500

    Day 52: Multiple Inheritance & MRO

---
+++ Day52-Multiple-Inheritance-&-MRO/Day52-Multiple-Inheritance-&-MRO.py
@@ -0,0 +1,28 @@
+# Day 52: Multiple Inheritance & MRO 
+class Vehicle:
+    def start(self):
+        print("Vehicle is starting")
+
+    def info(self):
+        print("This is a vehicle")
+
+
+class Flyable:
+    def fly(self):
+        print("This object can fly")
+
+    def info(self):
+        print("This is a flyable object")
+
+
+class FlyingCar(Vehicle, Flyable):
+    pass
+
+
+car = FlyingCar()
+
+car.start()
+car.fly()
+car.info()   # Python will call the method based on MRO
+
+print(FlyingCar.mro())
